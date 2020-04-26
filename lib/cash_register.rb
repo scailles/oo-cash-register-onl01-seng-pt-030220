e42fcefa416687c71ce74827eb0c774fef04fb7e
Shaina C <scailles23@gmail.com>
2020-04-26 03:01:10 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,43 @@
+class CashRegister
+  
+  attr_accessor :total, :discount, :quantity
+  
+  
+  
+  def initialize(discount = 0.to_f)
+    @total = 0.to_f
+    @discount = discount
+    @cart = []
+  end
+  
+  def add_item(title, price, quantity = 1)
+   @title = title
+   @cart << title
+   @total += price*quantity
+  end
+  
+  def apply_discount
+     if @discount == 0
+       return "There is no discount to apply."
+     else
+       total_discount = (discount/100.to_f)
+       @total = (@total- (@total * total_discount))
+       return "After the discount, the total comes to $#{@total.to_i}."
+     end
+  end
+    
+  
+   def items
+ 
+  end 
+      
+      
+  def void_last_transaction
+    
+  
+  
+  
+end
 
+
+  
