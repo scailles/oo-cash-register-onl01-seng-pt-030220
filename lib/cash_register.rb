1f86f22b3643ea1db4439915cd92b2699e319176
Shaina C <scailles23@gmail.com>
2020-04-26 03:24:27 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,20 +10,7 @@ class CashRegister
     @cart = []
   end
   
-  def add_item(title,price,quantity = 1)
-    if quantity>1
-      i=0
-      while i<quantity
-        @items << title
-        i+=1
-      end
-    else
-      @items << title
-    end
-    @total += price*quantity
-    @last_transaction_amount = @total
-    @total
-  end
+
   
   def apply_discount
      if @discount == 0
