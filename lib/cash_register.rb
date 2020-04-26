6036b94e91def5d020cec0c99b01f514e69bb40e
Shaina C <scailles23@gmail.com>
2020-04-26 03:24:09 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,20 @@ class CashRegister
     @cart = []
   end
   
-
+  def add_item(title,price,quantity = 1)
+    if quantity>1
+      i=0
+      while i<quantity
+        @items << title
+        i+=1
+      end
+    else
+      @items << title
+    end
+    @total += price*quantity
+    @last_transaction_amount = @total
+    @total
+  end
   
   def apply_discount
      if @discount == 0
